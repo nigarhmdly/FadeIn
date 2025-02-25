e513dd7366dcdc9fa8a102cbef5a8e87e627bb10
nigarr111 <nigarra-sp102@code.edu.az>
2025-02-25 18:13:11 +0400

    ftxsxsdcds

--- src/redux/slices/usersApiSlice.js
+++ src/redux/slices/usersApiSlice.js
@@ -1,5 +1,5 @@
 import { apiSlice } from "./apiSlice";
-const USERS_URL = "/api/users";
+const USERS_URL = "https://backend-server.com/api/users"; // Tam URL əlavə edirik
 
 export const userApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
